8dcaea31b1be5de19a789724e2376b34d5543d7c
michel-klein <michel.kps@outlook.com>
2021-08-06 19:33:53 -0300

    sidebar and text field added

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TextField, { Input } from '@material/react-text-field';
+import MaterialIcon from '@material/react-material-icon';
 
-import { Container } from './styles';
+import logo from '../../assets/logo.svg';
 
-const Home = () => <Container>Hello world!</Container>;
+import { Container, Search, Logo, Wrapper, Map } from './styles';
+
+const Home = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  return (
+    <Wrapper>
+      <Container>
+        <Search>
+          <Logo src={logo} alt="Logo do restaurante" />
+          <TextField
+            label="Pesquisar restaurantes"
+            outlined
+            trailingIcon={<MaterialIcon role="button" icon="search" />}>
+            <Input value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+          </TextField>
+        </Search>
+      </Container>
+      <Map />
+    </Wrapper>
+  );
+};
 
 export default Home;
